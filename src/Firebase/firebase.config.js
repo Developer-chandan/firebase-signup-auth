60353bc5e8de661deb78127a26c1218179da16df
Chandan sen <chandan2441139@gmail.com>
2023-10-04 12:51:56 +0600

    firebase add and update google auth

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3NUWNu4hb1fh-0vKMvmKYFoOh3RmBLz0",
+  authDomain: "fir-practice-9de9d.firebaseapp.com",
+  projectId: "fir-practice-9de9d",
+  storageBucket: "fir-practice-9de9d.appspot.com",
+  messagingSenderId: "493564381468",
+  appId: "1:493564381468:web:e80ca58d1fe4511217d2d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
