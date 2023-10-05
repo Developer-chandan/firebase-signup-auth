994b2275b231b6a3de3ecca8ba27bbab1deb3887
Chandan sen <chandan2441139@gmail.com>
2023-10-06 03:03:03 +0600

    update firebase auth

--- src/Compontent/Login/Login.jsx
+++ src/Compontent/Login/Login.jsx
@@ -1,54 +1,159 @@
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import app from "../../Firebase/firebase.config";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 
 
 const Login = () => {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="bg-white p-8 rounded shadow-md w-96">
-          <h2 className="text-2xl font-semibold mb-4">Login</h2>
-          <form>
-            <div className="mb-4">
-              <label htmlFor="email" className="block text-gray-700 font-medium">Email</label>
-              <input
-                type="email"
-                id="email"
-                className="w-full border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
-                placeholder="Enter your email"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="password" className="block text-gray-700 font-medium">Password</label>
-              <input
-                type="password"
-                id="password"
-                className="w-full border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
-                placeholder="Enter your password"
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white font-semibold py-2 rounded hover:bg-blue-600 transition duration-300"
-            >
-              Log In
-            </button>
-          </form>
-          <div className="mt-4">
-            <p className="text-center text-gray-500">Or log in with</p>
-            <div className="flex justify-center mt-2 space-x-2">
-              <button className=" text-white p-2   transition duration-300">
+  const auth = getAuth(app);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  const googleProvider = new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
+
+
+
+  const handleGoogleSignUp = () => {
+    setSuccess('');
+    setError('');
+
+
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user)
+        {
+          if (user) {
+            setSuccess('Register Successfully')
+          } else {
+            setError('Already have a user')
+          }
+        }
+        // ...
+      }).catch((error) => {
+
+        console.log(error)
+        setError(error);
+
+      });
+  }
+
+  const handleGitHubSignUp = () => {
+    setSuccess('');
+    setError('');
+    signInWithPopup(auth, gitHubProvider)
+      .then((result) => {
+        const user = result;
+        console.log(user);
+
+      }).catch(error => {
+        console.log(error);
+        setError(error);
+      })
+  }
+
+
+  const handleFacebookSignUp = () => {
+    console.log('Click from facebook auth')
+  }
+
+  const handleLogIn = (event) => {
+    setSuccess('');
+    setError('');
+    event.preventDefault();
+
+    const from = event.target;
+    const email = from.email.value;
+    const password = from.password.value;
+
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+       
+          const user = userCredential.user;
+          console.log(user);
+          setSuccess('login Successfull')
+        
+      
+        
+      })
+      .catch((error) => {
+        setError("User dosen't exist")
+        const errorMessage = error.message;
+        console.log('login: ', errorMessage)
+      });
+  }
+
+
+  // const handleSignOut = () => {
+  //   signOut(auth).then(() => {
+
+  //   }).catch((error) => {
+  //     // An error happened.
+  //   });
+  // }
+
+
+
+
+
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md w-96">
+        <h2 className="text-2xl font-semibold mb-4">Login</h2>
+        <form onSubmit={handleLogIn}>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 font-medium">Email</label>
+            <input
+              type="email"
+              id="email"
+              className="w-full border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700 font-medium">Password</label>
+            <input
+              type="password"
+              id="password"
+              className="w-full border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white font-semibold py-2 rounded hover:bg-blue-600 transition duration-300"
+          >
+            Log In
+          </button>
+          <div>
+            <p className="text-green-500">{success}</p>
+            <p className="text-red-500">{error}</p>
+          </div>
+        </form>
+        <div className="mt-4">
+          <p className="text-center text-gray-500">Or LogIn with</p>
+          <div className="flex justify-center mt-2 space-x-2">
+            <button onClick={handleGoogleSignUp} className=" text-white p-2   transition duration-300">
               <img className="w-[50px] h-[50px] rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnw_uBqjQDj4hLr5NRakpD2MOlqALQZHVNrxnsk3jZbKF8Ltd9aTU1OAeW_RaQSUvXoM8&usqp=CAU" alt="" />
-              </button>
-              <button className=" text-white p-2  transition duration-300">
-                <img className="w-[50px] h-[50px] rounded-full"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSevjLMoY69RrmdOeKZONLsRySpvdkEwP-SaraZ3hc_8WFS0eR1ALIIL9xuP2_nWbRnawY&usqp=CAU" alt="" />
-              </button>
-              <button className=" text-white p-2  transition duration-300">
+            </button>
+            <button onClick={handleGitHubSignUp} className=" text-white p-2  transition duration-300">
+              <img className="w-[50px] h-[50px] rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSevjLMoY69RrmdOeKZONLsRySpvdkEwP-SaraZ3hc_8WFS0eR1ALIIL9xuP2_nWbRnawY&usqp=CAU" alt="" />
+            </button>
+            <button onClick={handleFacebookSignUp} className=" text-white p-2  transition duration-300">
               <img className="w-[50px] h-[50px] rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs2BR3DOwlpXs4PhCvYWk7WsNZYKZ8PLvGj-VkngNxgELCcRsmCyLAhCAstlTZqBwBZIY&usqp=CAU" alt="" />
-              </button>
-            </div>
+            </button>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
--- src/Compontent/Register/Register.jsx
+++ src/Compontent/Register/Register.jsx
@@ -1,72 +1,179 @@
-import React from 'react';
+
+
+import { FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithPopup } from "firebase/auth";
+import app from "../../Firebase/firebase.config";
+import { useState } from "react";
+
+
+
 
 const Register = () => {
-    return (
-        <div>
-               <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
-        <h2 className="text-2xl font-semibold text-center mb-4">Register</h2>
-        <form className="space-y-4">
-          <div>
-            <label htmlFor="email" className="block text-left text-sm font-medium text-gray-700">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="mt-1 p-2 w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200"
-              placeholder="Your email"
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="text-left block text-sm font-medium text-gray-700">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="mt-1 p-2 w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200"
-              placeholder="Your password"
-            />
-          </div>
-          <div>
-            <label htmlFor="confirmPassword" className="text-left block text-sm font-medium text-gray-700">
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              id="confirmPassword"
-              className="mt-1 p-2 w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200"
-              placeholder="Confirm password"
-            />
-          </div>
-          <div className="flex justify-between items-center">
-            <button
-              type="submit"
-              className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
-            >
-              Register
-            </button>
-          </div>
-        </form>
-        <div className="mt-4">
-            <p className="text-center text-gray-500">Or log in with</p>
+
+  const auth = getAuth(app);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+
+  const googleProvider = new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
+  const facebookProvider = new FacebookAuthProvider();
+
+  const handleGoogleSignUp = () => {
+    setSuccess('');
+    setError('');
+  
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user)
+        {
+          if (user) {
+            setSuccess('Register Successfully')
+          }else{
+            setError('Already have a user')
+          }
+        }
+        // ...
+      }).catch((error) => {
+      
+        console.log(error)
+        setError(error);
+
+      });
+  }
+
+  const handleGitHubSignUp = () => {
+    signInWithPopup(auth, gitHubProvider)
+    .then((result) => {
+      const user = result;
+      console.log(user);
+      
+    }).catch (error => {
+      console.log(error);
+      setError(error);
+    })
+  }
+
+  const handleFacebookSignUp = () => {
+    signInWithPopup(auth, facebookProvider)
+    .then(result => {
+      const user = result;
+      console.log(user);
+    }).catch((error) => {
+      console.log(error);
+
+    })
+  }
+const handleRegister = (event)=>{
+  setSuccess('');
+  setError('');
+  event.preventDefault();
+  
+  const from = event.target;
+  const email = from.email.value;
+  const password = from.password.value;
+  const cpassword = from.passwordc.value;
+  createUserWithEmailAndPassword(auth, email, password, cpassword)
+  .then((userCredential) => {
+    // Signed up 
+    console.log(userCredential.user);
+    if(userCredential.user){
+      const user = userCredential.user;
+      console.log(user)
+      setSuccess('User Created Successfully')
+    }else if(userCredential.user.email == email){
+      setError('User Alreay Exist')
+    }
+    
+    // ...
+  }).catch(error => {
+    console.log(error.message)
+  })
+
+  
+
+
+
+}
+
+
+  return (
+    <div>
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
+          <h2 className="text-2xl font-semibold text-center mb-4">Register</h2>
+          <form className="space-y-4" onSubmit={handleRegister}>
+            <div>
+              <label htmlFor="email" className="block text-left text-sm font-medium text-gray-700">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="mt-1 p-2 w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200"
+                placeholder="Your email"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="text-left block text-sm font-medium text-gray-700">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="mt-1 p-2 w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200"
+                placeholder="Your password"
+              />
+            </div>
+            <div>
+              <label htmlFor="confirmPassword" className="text-left block text-sm font-medium text-gray-700">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                name="passwordc"
+                id="confirmPassword"
+                className="mt-1 p-2 w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200"
+                placeholder="Confirm password"
+              />
+            </div>
+            <div className="flex justify-between items-center">
+              <button
+                type="submit"
+                className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
+              >
+                Register
+              </button>
+            </div>
+            <div className='error'>
+          
+              <p>{success}</p>
+              <p>{error}</p>
+
+            </div>
+          </form>
+          <div className="mt-4">
+            <p className="text-center text-gray-500">Or LogIn with</p>
             <div className="flex justify-center mt-2 space-x-2">
-              <button className=" text-white p-2   transition duration-300">
-              <img className="w-[50px] h-[50px] rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnw_uBqjQDj4hLr5NRakpD2MOlqALQZHVNrxnsk3jZbKF8Ltd9aTU1OAeW_RaQSUvXoM8&usqp=CAU" alt="" />
+              <button onClick={handleGoogleSignUp} className=" text-white p-2   transition duration-300">
+                <img className="w-[50px] h-[50px] rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnw_uBqjQDj4hLr5NRakpD2MOlqALQZHVNrxnsk3jZbKF8Ltd9aTU1OAeW_RaQSUvXoM8&usqp=CAU" alt="" />
               </button>
-              <button className=" text-white p-2  transition duration-300">
-                <img className="w-[50px] h-[50px] rounded-full"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSevjLMoY69RrmdOeKZONLsRySpvdkEwP-SaraZ3hc_8WFS0eR1ALIIL9xuP2_nWbRnawY&usqp=CAU" alt="" />
+              <button onClick={handleGitHubSignUp} className=" text-white p-2  transition duration-300">
+                <img className="w-[50px] h-[50px] rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSevjLMoY69RrmdOeKZONLsRySpvdkEwP-SaraZ3hc_8WFS0eR1ALIIL9xuP2_nWbRnawY&usqp=CAU" alt="" />
               </button>
-              <button className=" text-white p-2  transition duration-300">
-              <img className="w-[50px] h-[50px] rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs2BR3DOwlpXs4PhCvYWk7WsNZYKZ8PLvGj-VkngNxgELCcRsmCyLAhCAstlTZqBwBZIY&usqp=CAU" alt="" />
+              <button onClick={handleFacebookSignUp} className=" text-white p-2  transition duration-300">
+                <img className="w-[50px] h-[50px] rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs2BR3DOwlpXs4PhCvYWk7WsNZYKZ8PLvGj-VkngNxgELCcRsmCyLAhCAstlTZqBwBZIY&usqp=CAU" alt="" />
               </button>
             </div>
           </div>
+        </div>
       </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default Register;
